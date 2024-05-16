b0ad4c0c62cc5af7d214ce6e0428216eaaace948
William <williamdsmithjr@gmail.com>
2024-05-15 20:55:19 -0500

    new push

---
+++ quiz.py
@@ -0,0 +1,31 @@
+# #Print “Hello World!” to the terminal
+# print (("Hello World!"))
+
+# #Create a variable with a number and print the variable to the screen
+# number="7"
+# print (number)
+
+# #Get the user to input a float number and convert to an integer
+# print(20)                  
+# print(20.)
+
+# #Create if else statement to see if user likes dogs
+# dogs = input("Do you like dogs\n")
+# if dogs == "yes":
+#     print("Lets play with the dog")
+# else:
+#     print("Run away")
+
+
+# #Create while loop to go as long as user input is not 5
+# user_input = int(input("Please enter a number"))
+# while user_input != 5:
+#     user_input = int(input("Please enter a number"))
+# else:
+#     print(("You chose right!"))
+
+
+# For loop
+for i in range(1, 10):
+    print("Goodnight")
+
