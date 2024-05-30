5714faf9365f003481bbc8c2ca5012c14b74cfc5
william-smith2024 <williamdsmithjr@gmail.com>
2024-05-29 19:30:17 -0500

    updated code

--- test.py
+++ test.py
@@ -1,15 +1,24 @@
 import random
 
-# The game will prompt the user to guess a number between 1 and 10, providing feedback on whether the guess is too high, too low, or correct. Use random. "import random"
+def guess_the_number():
+    # Generate a random number between 1 and 10
+    number_to_guess = random.randint(1, 10)
+    guess = None
 
-# Generate a random number between 1 and 10
-
-randomnumber = random.randint(1, 10)
-print(randomnumber)
-while randomnumber  > 0:
-    if randomnumber % 10 == 0:
-        print(randomnumber)
-    else:
-        print(randomnumber)
-
+    print("Welcome to the Guess the Number game!")
+    print("I have selected a number between 1 and 10.")
+    
+    while guess != number_to_guess:
+        # Ask the user to guess the number
+        guess = int(input("Please enter your guess: "))
+        
+        if guess < number_to_guess:
+            print("Too low! Try again.")
+        elif guess > number_to_guess:
+            print("Too high! Try again.")
+        else:
+            print("Congratulations! You guessed the correct number.")
+            
+# Run the game
+guess_the_number()
 
